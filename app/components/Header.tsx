45e958b7b67a2a7a27dc7c6231dbc83c1649027f
Karolina Gil <60822728+KaroGil@users.noreply.github.com>
2025-04-26 18:24:22 +0200

    add chat feature (#9)

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ import LoginButton from "./ButtonWithIcon/LoginButton";
 export default function Header() {
   return (
     <div className="flex justify-between items-center p-8">
-      <NavLink to={"/"}>
+      <NavLink to={"/"} className={"flex items-center space-x-2"}>
         <h1 className="font-bold text-2xl ">RIKSTEAM</h1>
       </NavLink>
       <div className="flex space-x-8 items-center">
