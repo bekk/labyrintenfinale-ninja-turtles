28a914ac6c458729982cf403e7736b5f14828bcb
KaroGil <karolinkagil@gmail.com>
2025-04-26 11:32:49 +0200

    add login form

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("/hjelp" ,"routes/help.tsx"),
+  route("/hjelp", "routes/help.tsx"),
   route("/more-info", "routes/more-info.tsx"),
+  route("/login", "routes/login.tsx"),
 ] satisfies RouteConfig;
