9adff1dfe9cd0f1478bb89c60749f0b8bc2aafda
KaroGil <karolinkagil@gmail.com>
2025-04-26 17:10:14 +0200

    add logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,11 @@ export default function Header() {
   return (
     <div className="flex justify-between items-center p-8">
       <NavLink to={"/"}>
-        <h1 className="font-bold text-2xl ">RIKSTEAM</h1>
+        <img
+          src="public/logo-transparent.png"
+          alt="Riksteam Logo"
+          className="h-32 w-32"
+        />
       </NavLink>
       <div className="flex space-x-4 items-center">
         <NavLink to="/advertisements">
