97a99bfb678847c1286be7bfa8271440bc2cbf45
Mille Hoeg <pik018@uib.no>
2025-04-26 17:54:50 +0200

    add video

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,31 +1,18 @@
-import { useEffect, useState } from "react";
-
-const backgroundImages = [
-  "/public/bergen.jpg",
-  "/public/bergen2.jpg",
-  "/public/lofoten.webp",
-];
+const backgroundVideo = "/tettstedet.mp4";
 
 export function HeroSection() {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImageIndex(
-        (prevIndex) => (prevIndex + 1) % backgroundImages.length
-      );
-    }, 5000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const currentImage = backgroundImages[currentImageIndex];
-
   return (
     <section
-      className="h-[80vh] w-full bg-cover bg-center flex items-center justify-center text-white text-center relative transition-all duration-1000"
-      style={{ backgroundImage: `url(${currentImage})` }}
+      className="h-[80vh] w-full flex items-center justify-center text-white text-center relative"
     >
+      <video
+        className="absolute inset-0 w-full h-full object-cover"
+        src={backgroundVideo}
+        autoPlay
+        loop
+        muted
+      />
+
       <div className="absolute inset-0 bg-black/50"></div>
 
       <div className="relative z-10 p-8">
