1657bfdff8887fe9924693e9c133773a8c1d0efa
Johanne Blikberg Herheim <joher2128@uib.no>
2025-04-26 12:58:44 +0200

    Fix routes

--- app/routes.ts
+++ app/routes.ts
@@ -4,14 +4,11 @@ export default [
   index("routes/home.tsx"),
   route("/hjelp", "routes/help.tsx"),
   route("/more-info", "routes/more-info.tsx"),
-<<<<<<< HEAD
   route("/om-oss", "routes/about-us.tsx"),
   route("/vårt-formål", "routes/our-goals.tsx"),
   route("/login", "routes/login.tsx"),
   route("/register", "routes/register.tsx"),
   route("/register/company", "routes/register/company.tsx"),
   route("/register/personal", "routes/register/personal.tsx"),
-=======
   route("/advertisements", "routes/advertisements.tsx"),
->>>>>>> 23e7e36 (start card)
 ] satisfies RouteConfig;
