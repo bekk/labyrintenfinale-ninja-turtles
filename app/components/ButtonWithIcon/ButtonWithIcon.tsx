869079ee8be21fb0c94cb58915063003cea22aac
KaroGil <karolinkagil@gmail.com>
2025-04-26 10:50:06 +0200

    add header

--- app/components/ButtonWithIcon/ButtonWithIcon.tsx
+++ app/components/ButtonWithIcon/ButtonWithIcon.tsx
@@ -1,20 +1,28 @@
-
 type Props = {
   text: string;
   onClick?: () => void;
   icon?: React.ReactNode;
   iconPlacement?: "left" | "right";
-}
+  className?: string;
+};
 
-export function ButtonWithIcon({ text, onClick, icon, iconPlacement = "left" }: Props) {
+export function ButtonWithIcon({
+  text,
+  onClick,
+  icon,
+  iconPlacement = "left",
+  className,
+}: Props) {
   return (
     <button
-      className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-3 rounded inline-flex items-center cursor-pointer gap-2"
+      className={`font-bold py-2 px-3 rounded inline-flex items-center cursor-pointer gap-2 ${
+        className ?? ""
+      }`}
       onClick={onClick}
     >
       {iconPlacement === "left" && icon}
       <span>{text}</span>
       {iconPlacement === "right" && icon}
     </button>
-  )
+  );
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,34 +1,14 @@
 import type { Route } from "./+types/home";
-import { ButtonWithIcon } from "~/components/ButtonWithIcon/ButtonWithIcon";
-import { NavLink, useNavigate } from "react-router";
-import QuestionMarkIcon from "~/icons/QuestionMarkIcon";
+import Header from "~/components/Header";
 
 export function meta({}: Route.MetaArgs) {
-  return [{ title: "Denne tittelen vises i fanen" }];
+  return [{ title: "RIKSTEAM" }];
 }
 
 export default function Home() {
-  let navigate = useNavigate();
-
   return (
     <>
-      <div className="pt-16 p-4 container mx-auto">
-        <p>
-          {" "}
-          Velkommen! Denne applikasjonen kan dere bruke for å vise frem ideen
-          deres!{" "}
-        </p>
-        <NavLink to="/more-info">
-          <a className="text-blue-600 underline hover:text-blue-800">Login</a>
-        </NavLink>
-      </div>
-      <div className="absolute right-10 bottom-10">
-        <ButtonWithIcon
-          text="Hjelp"
-          onClick={() => navigate("/hjelp")}
-          icon={<QuestionMarkIcon />}
-        />
-      </div>
+      <Header />
     </>
   );
 }
